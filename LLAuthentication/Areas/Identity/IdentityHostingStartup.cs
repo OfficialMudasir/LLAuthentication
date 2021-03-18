30a4e9bab071371ce1e7d39b98a7e92954410074
wani <itzahmad395@gmail.com>
2021-03-18 18:34:22 +0530

    Add project files.

---
+++ LLAuthentication/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using LLAuthentication.Areas.Identity.Data;
+using LLAuthentication.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(LLAuthentication.Areas.Identity.IdentityHostingStartup))]
+namespace LLAuthentication.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AuthDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AuthDbContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
+                    .AddEntityFrameworkStores<AuthDbContext>();
+
+                
+            });
+        }
+    }
+}
---
+++ LLAuthentication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LLAuthentication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
